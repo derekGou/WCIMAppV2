a25c7827f98a2090d31f5adffdf46baadf65433e
derekGou <derekgou19@gmail.com>
2024-11-26 17:24:48 -0500

    debugging firebase

--- api/incrementVisits.js
+++ api/incrementVisits.js
@@ -22,8 +22,7 @@ export default async function handler(req, res) {
     }
 
     if (req.method !== 'POST') {
-        console.log(req.method)
-        return res.status(405).json({ error: "Method not allowed" });
+        return res.status(405).json({ error: req.method + ' visit' });
     }
 
     try {
