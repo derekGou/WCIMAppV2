c99a2e16da76aa088baa4be09d802c22d94a75ca
derekGou <derekgou19@gmail.com>
2024-11-26 18:44:19 -0500

    grr

--- api/incrementVisits.js
+++ api/incrementVisits.js
@@ -23,9 +23,9 @@ export default async function handler(req, res) {
         return res.status(403).json({ error: "Unauthorized request" });
     }
 
-    if (req.method !== 'PUT') {
-        return res.status(405).json({ error: req.method + ' visit' });
-    }
+    // if (req.method !== 'PUT') {
+    //     return res.status(405).json({ error: req.method + ' visit' });
+    // }
 
     try {
         const docRef = db.collection('analytics').doc('clicks');
