6acfb4bca83e44c29faced1446cb786cb693ec6e
derekGou <derekgou19@gmail.com>
2024-11-26 17:21:57 -0500

    vercel+firebase test

--- api/incrementClicks.js
+++ api/incrementClicks.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
   }
 
   if (req.method !== 'POST') {
+    console.log(req.method + 'click')
     return res.status(405).json({ error: "Method not allowed" });
   }
 
--- api/incrementVisits.js
+++ api/incrementVisits.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
     }
 
     if (req.method !== 'POST') {
+        console.log(req.method)
         return res.status(405).json({ error: "Method not allowed" });
     }
 
