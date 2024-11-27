ade2e2ddf012b26c61e4e409ed710968cb27b4d0
derekGou <derekgou19@gmail.com>
2024-11-27 08:49:44 -0500

    plz work

--- api/incrementClicks.js
+++ api/incrementClicks.js
@@ -7,7 +7,7 @@ const app = initializeApp({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: 'derekgou19@gmail.com',
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
 });
 
