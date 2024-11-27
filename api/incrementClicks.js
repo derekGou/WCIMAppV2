29c393a6b9080a99c9347bdfa9af0b6264e1a8a2
derekGou <derekgou19@gmail.com>
2024-11-26 19:29:36 -0500

    awdw

--- api/incrementClicks.js
+++ api/incrementClicks.js
@@ -5,8 +5,8 @@ import { getFirestore } from 'firebase-admin/firestore';
 const app = initializeApp({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: 'derekgou19@gmail.com',
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
   }),
 });
 
@@ -25,25 +25,25 @@ export default async function handler(req, res) {
 
   // Verify request origin
 
-  // if (req.method !== 'PUT') {
-  //     return res.status(405).json({ error: `${req.method} not allowed` });
-  // }
+  if (req.method !== 'POST') {
+      return res.status(405).json({ error: `${req.method} not allowed` });
+  }
 
-  // try {
-  //   const docRef = db.collection('analytics').doc('visits');
-  //   await db.runTransaction(async (transaction) => {
-  //     const doc = await transaction.get(docRef);
-  //     if (!doc.exists) {
-  //       transaction.set(docRef, { val: 1 });
-  //     } else {
-  //       const currentVal = doc?.data()?.val || 0;
-  //       transaction.update(docRef, { val: currentVal + 1 });
-  //     }
-  //   });
+  try {
+    const docRef = db.collection('analytics').doc('visits');
+    await db.runTransaction(async (transaction) => {
+      const doc = await transaction.get(docRef);
+      if (!doc.exists) {
+        transaction.set(docRef, { val: 1 });
+      } else {
+        const currentVal = doc?.data()?.val || 0;
+        transaction.update(docRef, { val: currentVal + 1 });
+      }
+    });
 
-  //   return res.status(200).json({ message: "Visits incremented successfully" });
-  // } catch (error) {
-  //   console.error("Error incrementing visits:", error);
-  //   return res.status(500).json({ error: "Internal Server Error" });
-  // }
+    return res.status(200).json({ message: "Visits incremented successfully" });
+  } catch (error) {
+    console.error("Error incrementing visits:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 }
